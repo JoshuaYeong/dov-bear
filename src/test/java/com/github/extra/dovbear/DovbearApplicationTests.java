219b274542415ba646ba99bf4d635af20c6b0c9e
Joshua <joshua.asher.yeong@gmail.com>
2022-04-12 15:16:39 +0800

    first commit

---
+++ src/test/java/com/github/extra/dovbear/DovbearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.extra.dovbear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DovbearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
